2c0828744c704d936d98958997dfd4df1b8ac2aa
dskarthick97 <dskarthicksabari@gmail.com>
2021-08-10 15:07:01 +0530

    Handle exceptions

--- day_26_list_dict_comprehensions/main.py
+++ day_26_list_dict_comprehensions/main.py
@@ -9,6 +9,15 @@ df = pd.read_csv("nato_phonetic_alphabet.csv")
 df_dict = {row.letter: row.code for _, row in df.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-name = input("Enter a word: ")
-phontic_names = [df_dict.get(letter) for letter in name.upper()]
-print(phontic_names)
+def generate_phonetic():
+    name = input("Enter a word: ")
+    try:
+        phontic_names = [df_dict.get(letter) for letter in name.upper()]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phontic_names)
+
+
+generate_phonetic()
